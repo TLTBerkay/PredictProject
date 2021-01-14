6b80a8a2f21dfbbcc71b6b9cc750f43d197bbf02
TLTBerkayYıldırım <31593229+TLTBerkay@users.noreply.github.com>
2021-01-14 19:38:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -48,11 +48,6 @@ class base(Frame):
         e_term = Entry(inputsFrame)
         e_term.grid(row=1, column=1, stick="we", padx=(0, 5))
 
-        # Result labels
-        Label(inputsFrame,text='Accuracy : %', width=14).grid(row=2, column=0)
-        Label(inputsFrame, text='Debt $:', width=14).grid(row=3, column=0)
-        Label(inputsFrame, text='increase amount :', width=14).grid(row=4, column=0)
-
         # Loan Interest
         Label(inputsFrame, text="Monhtly Interest", width=14).grid(row=0, column=2)
         e_interest = Entry(inputsFrame)
@@ -60,7 +55,7 @@ class base(Frame):
 
         # Increase Foresee
         Label(inputsFrame, text="Commodity Type", width=14).grid(row=1, column=2)
-        e_commodity_options = ["", "MSFT", "AAPL", "IBM", "USDTRY=X", "EURTRY=X", "BTC-USD"]
+        e_commodity_options = ["", "MSFT", "AAPL", "IBM", "USDTRY=X", "EURTRY=X", "BTC-USD", "PETKM.IS", "TKC"]
         e_commodity_var = StringVar(inputsFrame)
         e_commodity_var.set(e_commodity_options[0])
         e_commodity = OptionMenu(inputsFrame, e_commodity_var, *e_commodity_options)
@@ -71,20 +66,16 @@ class base(Frame):
 
         def calculate():
 
-            # Dept detection
+            # Input variables.
             loan = int(e_loan.get())
             interest = float(e_interest.get())
             times = int(e_term.get())
-            dept = loan * interest * times / 100 + loan
-            print('Dept $', int(dept))
-            #<-------------------------------------------->#
+            commodity = e_commodity_var.get()
+
+            debt = loan * interest * times / 100 + loan
 
             # YahooFinance historical data
-            df = yf.download(e_commodity_var.get(), period="10y", interval="1d") #---> Data Size
-
-
-            close_px = df['Adj Close']
-            move_avg = close_px.rolling(100, min_periods=1).mean()
+            df = yf.download(commodity, period="10y", interval="1d") #---> Data Size
 
             dfreg = df.loc[:, ['Open', 'Close', 'Adj Close', 'Volume']]
             dfreg['HILO_PCT'] = (df['High'] - df['Low']) / df['Close'] * 100.0
@@ -92,24 +83,17 @@ class base(Frame):
             dfreg.fillna(value=-99999, inplace=True)
 
             #Forecastout = Calculation of the time
-            forecast_out = times * 30
-            print('toplam gün sayısı :', forecast_out)
-            #<------------------------------------------>#
+            forecast_out = times * 30         # month times product days 30 so get the total time days
 
-            print(dfreg)
-            dfreg['label'] = dfreg['Close'].shift(-forecast_out)
+            dfreg['label'] = dfreg['Close'].shift(-forecast_out) #label stununa close vaerilirini atıyoruz forecastout dan alına sure kadar
 
             # Calculation of the lot
             liste2 = list(dfreg['Close'][-1:])# YF'den alınan son close datası alım olarak kabul edilir
-            print('last index :', liste2)
-            loan = int(e_loan.get())
             lot = loan / int(liste2[0])
-            print('lot :', int(lot))
-            #<----------------------------->#
 
             # Separation
             x = dfreg.drop(columns='label')
-            x = scale(x)          # label dışındaki bütün verileri belli parametereye yerleştiriyor.
+            x = scale(x) # label dışındaki bütün verileri belli parametereye yerleştiriyor.
 
             y = dfreg.iloc[:, -1]  # labeldaki tüm satırları al,en son sütunu al
             x_to_predict = x[-forecast_out:]  # the last day we will guess
@@ -125,7 +109,6 @@ class base(Frame):
 
             # Percentage of Accuracy
             accuracy = regressor.score(x_test, y_test) * 100
-            print('Accuracy : %', accuracy)
 
             prediction_set = regressor.predict(x_to_predict)
             dfreg['Prediction'] = np.nan
@@ -144,10 +127,8 @@ class base(Frame):
                 dfreg.loc[next_date] = [np.nan for q in range(len(dfreg.columns) - 1)] + [i]
 
             # Last and First Predict detection
-            print('prediction list', list(dfreg['Prediction'][-forecast_out:]))
             firstPredict = list(dfreg['Prediction'][-forecast_out:-forecast_out + 1]) #----> ilk predict değerini listeden bulmka için -forecast:-forecast+1 yaparak predict forecastin ilk değerini buluruz.
             lastPredict = list(dfreg['Prediction'][-1:])
-            print('LastPredict :', lastPredict, 'FirstPredict :', firstPredict)
 
             # Calculation of increase amount
             liste = lastPredict + firstPredict
@@ -155,22 +136,26 @@ class base(Frame):
             b = liste[1]
             result = b - a
             increase = result / a * -1 * 100
-            print('increase amount: %', increase)
+            
+            #calculation of total profit
+            total_profit = loan * increase - debt
+
 
             # Calculation of new list = lot X predict
             liste3 = list(dfreg['Prediction'][-forecast_out:])
             liste3 = [i * lot for i in liste3]
-            print('Prediction list X Lot :', liste3)
 
             # Output labels
-            Label(inputsFrame, text=accuracy, width=14).grid(row=2, column=1)
-            Label(inputsFrame, text=dept, width=14).grid(row=3, column=1)
-            Label(inputsFrame, text=increase, width=14).grid(row=4, column=1)
+            Label(inputsFrame, text="Accuracy: {:.2f}%".format(accuracy), width=14).grid(row=2, column=0)
+            Label(inputsFrame, text="Debt: ${:.2f}".format(debt), width=14).grid(row=2, column=1)
+            Label(inputsFrame, text="Change: {:.2f}%".format(increase), width=14).grid(row=2, column=2)
+            Label(inputsFrame, text="Total Profit : {:.2f}%".format(totalm), width=14).grid(row=2, column=3)
+
 
             # Plot Stuff
             predict_plot.clear()
             predict_plot.plot(liste3)
-            predict_plot.plot([0, forecast_out], [loan, dept])
+            predict_plot.plot([0, forecast_out], [loan, debt])
 
             canvas.draw()
 
